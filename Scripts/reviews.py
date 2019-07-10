2bcf2daade2779ad72d213a794483fe6a3569b11
sasha <gsancho0001@gmail.com>
2019-07-10 19:56:22 +0300

    reviews

--- Scripts/reviews.py
+++ Scripts/reviews.py
@@ -1,25 +1,33 @@
 from selenium import webdriver
+from os import listdir
 import time
 
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument("--test-type")
-driver = webdriver.Chrome(chrome_options=options)
-driver.get('https://jobs.dou.ua/companies/ciklum/reviews/#archive')
+driver = webdriver.Chrome('chromedriver.exe')
 
-element = driver.find_element_by_link_text("Архив отзывов")
-element.click()
+linksFile = open('links.txt', 'r')
+links = linksFile.read().split()
+files_list = listdir()
 
-while True:
-    try:
-        element = driver.find_element_by_link_text("Показать еще")
-        element.click()
-        time.sleep(2)
-    except:
-        break
-    
-page_source = driver.page_source
-file = open('some.html', 'w')
-file.write(page_source)
-file.close()
-    
+for link in links:
+    company_name = link.split('/')[-2]
+    if company_name not in files_list:
+        try:
+            driver.get(link + 'reviews/')
+            element = driver.find_element_by_link_text("Архив отзывов")
+            element.click()
+        except:
+            print('Done1 ' + company_name)
+        while True:
+            try:
+                element = driver.find_element_by_link_text("Показать еще")
+                element.click()
+                time.sleep(2)
+            except:
+                print('Done2 ' + company_name)
+                break
+        page_source = driver.page_source.encode("utf-8")
+        file = open(company_name, 'wb')
+        file.write(page_source)
+        file.close()
+    else:
+        continue
