a1b8e549c1a11a7d8cca62ddff70ebaaf1653d80
Sasha <gsancho0001@gmail.com>
2019-08-05 21:09:22 +0300

    docs

--- DOU Companies/Program.fs
+++ DOU Companies/Program.fs
@@ -12,19 +12,30 @@ open System.Threading
 open GoogleNLP.Implementations
 open System.Linq
 
-let dirInfo = new DirectoryInfo(Directory.GetCurrentDirectory() + "../../../../App_Data/Reviews")
-let files = dirInfo.GetFiles ()
-let companyScores = new Dictionary<string, string>()
+let serializedText = File.ReadAllText("../../../App_Data/CompanyReviews/scores.json")
+let companyScores = JsonConvert.DeserializeObject<Dictionary<string, string>>(serializedText)
 
-for f in files do
-    try
-        let score = GetCompanyScore (f.FullName)
-        let result = companyScores.TryAdd(f.Name, score)
-        printfn "%s" f.Name
-    with
-    | _ -> ()
+let serializedText1 = File.ReadAllText("../../../App_Data/CompanyReviews/analysedReviews.json")
+let analysedReviews = JsonConvert.DeserializeObject<Dictionary<string, decimal[]>>(serializedText1)
 
-let scoresSerialized = JsonConvert.SerializeObject(companyScores)
-File.WriteAllText("../../../App_Data/CompanyReviews/scores.json", scoresSerialized)
+let mutable negativeReviewsCount = 0
+let mutable neutralReviewsCount = 0
+let mutable positiveReviewsCount = 0
+
+for kvp in analysedReviews do 
+    negativeReviewsCount <- negativeReviewsCount + kvp.Value.ToList().Where(fun r -> r < 0m).Count()
+    positiveReviewsCount <- positiveReviewsCount + kvp.Value.ToList().Where(fun r -> r > 0m).Count()
+    neutralReviewsCount <- neutralReviewsCount + kvp.Value.ToList().Where(fun r -> r = 0m).Count()
+
+let data =[
+    "Positive", positiveReviewsCount
+    "Negative", negativeReviewsCount
+    "Neutral", neutralReviewsCount
+]
+
+data |> Chart.Pie
+|> Chart.WithTitle "Reviews"
+|> Chart.WithLegend true
+|> Chart.Show
 
 Console.ReadKey ()
