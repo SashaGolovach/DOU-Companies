257dfda5c07299c2e33310e15d38837671222641
Sasha Golovach <gsancho0001@gmail.com>
2019-08-21 11:46:27 +0300

    heroku build fix

--- Dou-Companies-Front/server.js
+++ Dou-Companies-Front/server.js
@@ -1,16 +1,8 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/lib-demo'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/lib-demo/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 80);
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/lib-demo'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
